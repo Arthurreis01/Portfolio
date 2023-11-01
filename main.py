2934f7a39d0cc2ea9d9bd05e4bc20331ddf392f7
Arthurreis01 <147757007+Arthurreis01@users.noreply.github.com>
2023-11-01 12:15:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Olá, Antônio!")
-st.markdown('**``Segue em anexo algumas das minhas especificações:`**')
+st.markdown('**Segue em anexo algumas das minhas especificações:**')
 
 st.write(" - Sou formado pela Universidade Estácio de Sá em Administração e Gestão Financeira.")
 st.write(" - Possuo fluência em Português e Inglês.")
@@ -11,7 +11,7 @@ st.write(" - Nível intermediário em análise de dados em python e desenvolvime
 st.text("")
 st.text("")
 
-st.write(" **- `Abaixo, são alguns dos meus projetos que realizei utilizando o python:`**")
+st.write(" **- Abaixo, são alguns dos meus projetos que realizei utilizando o python:**")
 st.write("Aqui está exemplo de uma análise de dados feita sobre a guerra entre Israel e Palestina: (https://duz39dveyxciyxqf66sjdg.streamlit.app/)")
 st.write("Aqui está exemplo de Dashboard Financeiro de uma empresa: (https://dashboardfinanceiro.streamlit.app/)")
 st.write("Aqui está exemplo de uma coleta de dados do site Globo.com: (https://coletadedadosglobo.streamlit.app/")
