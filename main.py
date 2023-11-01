9b49a5ed9a7830f6bfd44e445665200f2d8d5040
Arthurreis01 <147757007+Arthurreis01@users.noreply.github.com>
2023-11-01 11:52:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.title("Olá, Antônio!")
+st.markdown('**Segue em anexo algumas das minhas especificações:**')
+
+st.write(" - Sou formado pela Universidade Estácio de Sá em Administração e Gestão Financeira.")
+st.write(" - Possuo fluência em Português e Inglês.")
+st.write(" - Nível intermediário em análise de dados em python e desenvolvimento web.")
+st.text("")
+st.text("")
+
+st.write(" **- Abaixo, são alguns dos meus projetos que realizei utilizando o python:**")
+st.write("Aqui está exemplo de uma análise de dados feita sobre a guerra entre Israel e Palestina: (https://duz39dveyxciyxqf66sjdg.streamlit.app/)")
+st.write("Aqui está exemplo de Dashboard Financeiro de uma empresa: (https://dashboardfinanceiro.streamlit.app/)")
+st.write("Aqui está exemplo de uma coleta de dados do site Globo.com: (https://coletadedadosglobo.streamlit.app/")
+st.write("Aqui está exemplo de uma coleta de informações sobre o mercado de futuros do dólar para fins de pesquisa pessoal: (https://dolarfuturo.streamlit.app/")
+
+st.write(" **- Todos são apenas ideias de funcionalidades que podemos adequar ao seu cotidiano e dos seus clientes**")
